016b1f096861412edf53db3d2499c8011e0cbcf6
AyaIrshaid <a.yirshaid@gmail.com>
2025-08-25 23:40:06 +0300

    inital commit

---
+++ Carousel/Carousel/CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselApp.swift
+//  Carousel
+//
+//  Created by Aya Irshaid on 25/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
